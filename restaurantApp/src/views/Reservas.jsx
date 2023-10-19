1504daae6d9695e4159ade963a36f33993534b47
Lucas <lucasfernandezc@outlook.com>
2023-10-19 00:04:42 -0300

    creando seccion reservas

--- restaurantApp/src/views/Reservas.jsx
+++ restaurantApp/src/views/Reservas.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import MyDatePicker from "../components/MyDatePicker";
+import { InputGroup } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
-const Reservas = () =>{
-    return(
-        <h1>Soy tu Reserva</h1>
+const Reservas = () => {
+    return (
+        <div id="inputsContainer">
+            <div>
+                <InputGroup style={{marginBottom:'20px'}}>
+                    <InputGroup.Text id="basic-addon1">Nombre</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Nombre"
+                        aria-label="Nombre"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </div>
+            <div>
+                <InputGroup>
+                    <InputGroup.Text id="basic-addon1">Apellido</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Apellido"
+                        aria-label="Apellido"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </div>
+            <div id="MyDatePicker">
+                <MyDatePicker/>
+            </div>
+            <div id="submitButton">
+            <Button variant="primary">Enviar</Button>
+            </div>
+        </div>
     )
 }
 
